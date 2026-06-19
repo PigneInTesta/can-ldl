459e4d7abfa2be32b8e2bed6cd726d921c6bbb1d
PigneInTesta <giovannitassotti165@gmail.com>
2026-06-19 12:17:38 +0200

    Change .csn format including also the data types to be converted and modify the parsing of the datas

--- include/can.hpp
+++ include/can.hpp
@@ -7,20 +7,33 @@
 
 #define CAN_SIGNAL_NAME_MAX 32
 #define CAN_SIGNAL_UNIT_MAX 8
+#define CAN_SIGNAL_TYPE_MAX 8
+
+enum class SignalType : uint8_t {
+    UINT8,
+    INT8,
+    UINT16,
+    INT16,
+    UINT32,
+    INT32
+};
 
 class CanSignal {
 public:
     CanSignal(uint16_t id, uint8_t size, uint8_t start, uint8_t freq, 
-        const char* name, double offset, double scaler, const char* unit)
+        const char* name, double offset, double scaler, const char* unit, SignalType type)
         : m_can_id(id)
         , m_size(size)
         , m_bit_start(start)
         , m_frequency(freq)
         , m_offset(offset)
         , m_scaler(scaler)
+        , m_type(type)
         , m_payload(0)
     {
+        #ifdef DEBUG
         printf("[CTOR] %s scaler=%f\n", name, scaler);
+        #endif
         strncpy(m_name, name, CAN_SIGNAL_NAME_MAX-1);
         m_name[CAN_SIGNAL_NAME_MAX-1] = '\0';
         strncpy(m_unit, unit, CAN_SIGNAL_UNIT_MAX-1);
@@ -34,6 +47,7 @@ public:
         , m_frequency(other.m_frequency)
         , m_offset(other.m_offset)
         , m_scaler(other.m_scaler)
+        , m_type(other.m_type)
         , m_payload(other.m_payload.load())
     {
         strncpy(m_name, other.m_name, CAN_SIGNAL_NAME_MAX);
@@ -48,12 +62,23 @@ public:
     double getOffset()      const { return m_offset; }
     double getScaler()      const { return m_scaler; }
     const char* getUnit()   const { return m_unit; }
+    SignalType getType()    const { return m_type; }
     uint64_t getPayload()   const { return m_payload.load(); }
 
     void setPayload(uint64_t payload) { m_payload.store(payload); }
 
     double getPhysicalValue() const {
-        return (static_cast<double>(m_payload.load()) * m_scaler) + m_offset;
+        uint64_t raw = m_payload.load();
+        double value;
+
+        switch (m_type){
+            case SignalType::INT8:  value = static_cast<double>(static_cast<int8_t>(raw)); break;
+            case SignalType::INT16: value = static_cast<double>(static_cast<int16_t>(raw)); break;
+            case SignalType::INT32: value = static_cast<double>(static_cast<int32_t>(raw)); break;
+            default:                value = static_cast<double>(raw);
+        }
+
+        return (value * m_scaler) + m_offset;
     }
 
 private:
@@ -65,6 +90,7 @@ private:
     const double m_offset;
     const double m_scaler;
     char m_unit[CAN_SIGNAL_UNIT_MAX];
+    const SignalType m_type;
 
     std::atomic<uint64_t> m_payload;
 };
--- src/can_config.cpp
+++ src/can_config.cpp
@@ -1,5 +1,15 @@
 #include "can_config.hpp"
 
+static SignalType parseType(const char* type_str) {
+    if (strcmp(type_str, "int8")   == 0) return SignalType::INT8;
+    if (strcmp(type_str, "int16")  == 0) return SignalType::INT16;
+    if (strcmp(type_str, "int32")  == 0) return SignalType::INT32;
+    if (strcmp(type_str, "uint8")  == 0) return SignalType::UINT8;
+    if (strcmp(type_str, "uint16") == 0) return SignalType::UINT16;
+    if (strcmp(type_str, "uint32") == 0) return SignalType::UINT32;
+    return SignalType::UINT8;
+}
+
 /**
  * Load the .csv file containing all the CAN messages with their respective
  * frequency.
@@ -30,11 +40,10 @@ bool CAN_LoadConfig(const std::string& filename, std::vector<FrequencyGroup>& gr
         char name[CAN_SIGNAL_NAME_MAX] = "";
         double offset = 0.0, scaler = 1.0;
         char unit[CAN_SIGNAL_UNIT_MAX] = "";
+        char type_str[CAN_SIGNAL_TYPE_MAX] = "uint8";
 
-        int ret = sscanf(line, "0x%hx,%hhu,%hhu,%hhu,%31[^,],%lf,%lf,%7s", 
-            &can_id, &size, &start, &freq, name, &offset, &scaler, unit);
-
-        printf("[PARSE] name=%s offset=%f scaler=%f unit=%s ret=%d\n", name, offset, scaler, unit, ret);
+        int ret = sscanf(line, "0x%hx,%hhu,%hhu,%hhu,%31[^,],%lf,%lf,%7[^,],%7s", 
+            &can_id, &size, &start, &freq, name, &offset, &scaler, unit, type_str);
         
         if (ret < 7) {
             // Not an error - could be an empty line
@@ -52,7 +61,11 @@ bool CAN_LoadConfig(const std::string& filename, std::vector<FrequencyGroup>& gr
             continue;
         }
 
-        // printf("[DEBUG] Line %d: 0x%hX,%hhu,%hhu,%hhu\n", line_number, can_id, size, start, freq, name, offset, scaler, unit);
+        #ifdef DEBUG
+        printf("[DEBUG] Line %d: 0x%hX,%hhu,%hhu,%hhu\n", line_number, can_id, size, start, freq, name, offset, scaler, unit);
+        #endif
+
+        SignalType type = parseType(type_str);
 
         // Checks if there is an existing group with same frequency as the current signal
         auto it = std::find_if(groups.begin(), groups.end(),
@@ -64,11 +77,11 @@ bool CAN_LoadConfig(const std::string& filename, std::vector<FrequencyGroup>& gr
         if (it == groups.end()) {
             // Create a new group
             FrequencyGroup group(freq);
-            group.m_signals.emplace_back(can_id, size, start, freq, name, offset, scaler, unit);
+            group.m_signals.emplace_back(can_id, size, start, freq, name, offset, scaler, unit, type);
             groups.push_back(std::move(group));
         } else {
             // Append the current signal to an existing one
-            it->m_signals.emplace_back(can_id, size, start, freq, name, offset, scaler, unit);
+            it->m_signals.emplace_back(can_id, size, start, freq, name, offset, scaler, unit, type);
         }
     }
 
